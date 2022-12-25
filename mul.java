c0e4ad80d04dfdadadb81b786aaf50074bd6f737
samma gopi kamadula <viratgopi147@gmail.com>
2022-12-25 23:22:54 +0530

    newly changed for mul.java

--- mul.java
+++ mul.java
@@ -1,6 +1,9 @@
 public class mul {
     public static void main(String [] args){
-        System.out.println("this is mul"); 
+        int n=1;
+        int n1=2;
+        
+        System.out.println(n*n1);      
 
     }
     
